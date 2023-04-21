6f6e379816e64bfe0f386ed410c6811276a15f4f
sahil <jainsahil1116@gmail.com>
2023-04-21 12:52:46 +0530

    changes

--- server/views.py
+++ server/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import flwr as fl
 
 
 # Create your views here.
@@ -8,3 +9,28 @@ def index(request):
         return render(request, "index.html")
     else:
         return HttpResponse("Invalid request")
+
+
+import threading
+import flwr
+
+from django.shortcuts import render
+from django.views import View
+
+
+class FLwrView(View):
+    template_name = 'flwr.html'
+
+    def get(self, request):
+       
+        t = threading.Thread(target=self.start_flwr_server)
+        t.start()
+
+
+        context = {'message': 'FLwr server started'}
+        return render(request, self.template_name, context)
+
+    def start_flwr_server(self):
+        strategy = flwr.server.strategy.FedAvg(min_num_clients=1)
+        flwr.server.start_server(server_address="0.0.0.0:8080", config=flwr.server.ServerConfig(num_rounds=3),strategy=strategy)
+        
