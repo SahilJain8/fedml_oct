0cf92ba425f46af0f9f8dd54615d793a8fcaa9c4
sahil <jainsahil1116@gmail.com>
2023-04-21 12:23:28 +0530

    first

---
+++ fedML_OCT/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fedML_OCT project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fedML_OCT.settings')
+
+application = get_wsgi_application()
