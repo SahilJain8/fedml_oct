dd2c628517ad30005e47afc4921dd9384a7276d1
sahil <jainsahil1116@gmail.com>
2023-04-21 12:52:55 +0530

    changes

--- fedML_OCT/settings.py
+++ fedML_OCT/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = load_dotenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-2bxg3gna-agf@#lgl#$@jyn*xan5=fn6=-2#646*n2oxfyox8x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- server/urls.py
+++ server/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,FLwrView
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('server/',FLwrView.as_view())
 ]
