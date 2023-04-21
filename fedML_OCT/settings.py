26ee17eab30ddd0b86f51fd1e11b023adbf28cf3
sahil <jainsahil1116@gmail.com>
2023-04-21 12:33:01 +0530

    changes

--- fedML_OCT/settings.py
+++ fedML_OCT/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2bxg3gna-agf@#lgl#$@jyn*xan5=fn6=-2#646*n2oxfyox8x'
+SECRET_KEY = load_dotenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
