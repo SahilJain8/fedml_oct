9462e5ec16eafaae29b629b577619bfdf7fc8599
sahil <jainsahil1116@gmail.com>
2023-04-22 21:26:33 +0530

    changes

--- client/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from django import forms
-
-class UploadImagesForm(forms.Form):
-    class1_images = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
-    class2_images = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
-    class3_images = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
-    class4_images = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
