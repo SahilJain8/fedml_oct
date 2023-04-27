f3b4d370cecb77f8554210b82af253d576d34bcc
sahil <jainsahil1116@gmail.com>
2023-04-27 12:00:51 +0530

    changes

--- client/views.py
+++ client/views.py
@@ -2,6 +2,18 @@ from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
 from .forms import ImageUploadForm
+import numpy as np
+from PIL import Image
+import io
+import tensorflow as tf
+import numpy as np
+
+
+# dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+
+# # Shuffle and batch the dataset
+# dataset = dataset.shuffle(buffer_size=len(images))
+# dataset = dataset.batch(batch_size)
 
 
 class IndexView(View):
@@ -16,9 +28,24 @@ def upload(request):
             dme_image = form.cleaned_data['dme_image']
             drusen_image = form.cleaned_data['drusen_image']
             normal_image = form.cleaned_data['normal_image']
-         
-            return HttpResponse("Succesfully")
+            cnv_image = np.array(Image.open(io.BytesIO(cnv_image.read())))
+            dme_image = np.array(Image.open(io.BytesIO(dme_image.read())))
+            drusen_image = np.array(Image.open(io.BytesIO(drusen_image.read())))
+            normal_image = np.array(Image.open(io.BytesIO(normal_image.read())))
+
+            images = np.stack([cnv_image, dme_image, drusen_image, normal_image], axis=0)
+            labels = np.array([0, 1, 2, 3])
+
+            # Create TensorFlow dataset
+            dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+            dataset = dataset.shuffle(buffer_size=len(images))
+            dataset = dataset.batch(32)
+
+           
+            print(dataset)
+
+            # Return response
+            return HttpResponse("Successfully uploaded images.")
     else:
         form = ImageUploadForm()
     return render(request, 'client/upload.html', {'form': form})
-
