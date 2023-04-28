89c035d9456fb4fdf0b852293f4ebfd3be34540e
sahil <jainsahil1116@gmail.com>
2023-04-28 08:14:49 +0530

    changes

---
+++ app1/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
+email_id = ""
+senders_name = ""
+
+
+
+# Create your views here.
+@login_required(login_url='login')
+def HomePage(request):
+    print(email_id)
+    return render(request, 'index.html')
+
+
+def SignupPage(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+
+        if pass1 != pass2:
+            return HttpResponse("Your password and conform password are not Same!!")
+        else:
+
+            my_user = User.objects.create_user(uname, email, pass1)
+            my_user.save()
+            return redirect('login')
+
+    return render(request, 'signup.html')
+
+
+def LoginPage(request):
+    global email_id, senders_name
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass')
+        user = authenticate(request, username=username, password=pass1)
+        senders_name = username
+        if user is not None:
+            if user.is_active:
+                request.session.set_expiry(86400)  # sets the exp. value of the session
+                login(request, user)
+                my_user = User.objects.filter(username__exact=username).values("email")
+                email_id = my_user[0]["email"]
+                print(email_id)
+             
+                return redirect('home')
+    
+
+        else:
+            return HttpResponse("Username or Password is incorrect!!!")
+
+    return render(request, 'login.html')
+
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
+
+
+
+
--- fedML_OCT/urls.py
+++ fedML_OCT/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from app1 import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("client.urls"))
+    path('',include("client.urls")),
+    path('', views.SignupPage, name='signup'),
+    path('login/', views.LoginPage, name='login'),
+  path('logout/', views.LogoutPage, name='logout'),
+
    
    
 ]
