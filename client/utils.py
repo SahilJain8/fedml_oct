8b70bf95b4124a330834235f82a0916320ba3180
jains <jainsahil1116@gmail.com>
2023-05-23 07:23:16 +0530

    chnages

--- client/utils.py
+++ client/utils.py
@@ -18,6 +18,50 @@ unique_id = str(uuid.uuid4())
 from app1.views import rms
 
 
+def my_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(192, 192, 3)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(4, activation='softmax')
+    ])
+    return model
+
+
+
+client = MongoClient('mongodb+srv://doadmin:y2Z3X0vSt4r568w7@db-mongodb-blr1-09187-cfe31e33.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-blr1-09187')
+db = client['fedml']
+weights_collection = db['client_model_weights']
+documents = weights_collection.find()
+
+model_weights_list = []
+
+for document in documents:
+    model_weights_bytes = document['model_weights']
+    model_weights_np = np.frombuffer(model_weights_bytes, dtype=np.float32)
+    model_weights_list.append(model_weights_np)
+
+
+def fedagg():
+    average_weights = np.mean(model_weights_list, axis=0)
+    model = my_model()
+
+    model.set_weights(average_weights)
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                  metrics=['accuracy'])
+
+    model.save('trained_model.h5')
+
+
+
+
+
 def create_image(image_byte):
     img_list = []
     for img in image_byte:
@@ -52,5 +96,6 @@ def create_model(dataset):
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email_id]
     send_mail( subject, message, email_from, recipient_list )
-
+    fedagg()
+   
 
